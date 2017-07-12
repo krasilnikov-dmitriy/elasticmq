1c622011f5af829ca53bba22035b34ed244d15a7
Dmitry Krasilnikov <krasilnikov.dmitriy@gmail.com>
2017-07-12 16:46:21 +0300

    configure checkout

--- jenkins/developerPipeline.groovy
+++ jenkins/developerPipeline.groovy
@@ -1,6 +1,10 @@
 node("mesos:hseeberger/scala-sbt:scala-2.12.2-sbt-0.13.15") {
     checkout scm
 
+    stage("check free space") {
+        sh "df -h"
+    }
+
     stage("compile") {
         sh "sbt elasticmq-server:assembly"
     }
